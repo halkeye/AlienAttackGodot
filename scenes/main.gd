f1d0dc439f02a2015111ba7270e09ca8e7908a1d
Gavin Mogan <github@gavinmogan.com>
2023-11-18 11:28:58 -0800

    Manually adjust walls for now, auto can come later

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,23 +2,20 @@ extends Node2D
 
 @export var bullet_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+#	var viewport = get_viewport_rect()
+#	$"LeftWall/CollisionShape2D".shape.size.x = 4
+#	$"LeftWall/CollisionShape2D".shape.size.y = viewport.size.y
+#	$"LeftWall/CollisionShape2D".position.x = 2
+#	$"LeftWall/CollisionShape2D".position.y = viewport.size.y / 2
+#
+#	$"RightWall/CollisionShape2D".shape.size.x = 4
+#	$"RightWall/CollisionShape2D".shape.size.y = viewport.size.y
+#	$"RightWall/CollisionShape2D".position.x = viewport.size.x - 2
+#	$"RightWall/CollisionShape2D".position.y = viewport.size.y / 2
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var viewport = get_viewport_rect()
-
-	$"LeftWall/CollisionShape2D".shape.size.x = 4
-	$"LeftWall/CollisionShape2D".shape.size.y = viewport.size.y
-	$"LeftWall/CollisionShape2D".position.x = 2
-	$"LeftWall/CollisionShape2D".position.y = viewport.size.y / 2
-	
-	$"RightWall/CollisionShape2D".shape.size.x = 4
-	$"RightWall/CollisionShape2D".shape.size.y = viewport.size.y
-	$"RightWall/CollisionShape2D".position.x = viewport.size.x - 2
-	$"RightWall/CollisionShape2D".position.y = viewport.size.y / 2
 	pass
 
 func _on_right_wall_body_entered(body):
