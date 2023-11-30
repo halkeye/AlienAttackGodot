48080b69c4fba6749b2c3d1a525cbc5da2e264a5
Gavin Mogan <github@gavinmogan.com>
2023-11-29 23:19:32 -0800

    Remastered Logo

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,15 +2,11 @@ extends MarginContainer
 
 var MainScene = preload("res://scenes/main.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
-
 
 func _on_new_game_pressed():
 	get_tree().change_scene_to_packed(MainScene)
